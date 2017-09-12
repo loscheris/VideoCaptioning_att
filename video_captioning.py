15d1d262de5a922881c5e8d3d40a93e7c8ec7f84
ben <bzy1993@hotmail.com>
2017-09-12 15:13:27 +0100

    delete add to database from video_captioning.py

--- video_captioning.py
+++ video_captioning.py
@@ -3,7 +3,7 @@ import os.path
 if os.path.basename(os.getcwd()) != 'web_VideoCaptioning':
     os.chdir('../web_VideoCaptioning')
 import utils
-from pymongo import MongoClient
+
 
 #Parse argument
 parser = argparse.ArgumentParser()
@@ -25,12 +25,3 @@ video_feat = utils.extract_video_features(video_full_path)
 generated_sentence = utils.get_caption(video_feat)
 
 print type(generated_sentence)
-#update mongodb
-client = MongoClient('mongodb://localhost:27017/')
-db = client.video_objects
-collection = db.videos
-video_file_name = os.path.basename(video_full_path)
-db_query = {"v_name": os.path.splitext(video_file_name)[0]}
-db_caption = {"$set" : {"caption": generated_sentence}}
-collection.update_many(db_query, db_caption)
-print "Caption added to DB"
